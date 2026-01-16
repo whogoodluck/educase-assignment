eca3ad7653598fd96f351637b371ba1719a592f8
sunny <goodluck14115@gmail.com>
2026-01-16 17:19:21 +0530

    Setup DB

---
+++ prisma/migrations/20260116114758_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "companyName" TEXT,
+    "isAgency" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,6 +4,11 @@ config()
 
 const PORT = process.env.PORT || 3001
 
+const DATABASE_URL = process.env.DATABASE_URL
+const DEV_DATABASE_URL = process.env.DEV_DATABASE_URL
+
 export default {
   PORT,
+  DATABASE_URL,
+  DEV_DATABASE_URL,
 }
