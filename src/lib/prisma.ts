7bf4b1a657ca5af385a7c901f4a49863ae5559b3
sunny <goodluck14115@gmail.com>
2026-01-16 17:50:33 +0530

    Refactor

---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../app/generated/prisma/client'
+import config from '../utils/config'
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
+
+const adapter = new PrismaPg({
+  connectionString: config.DATABASE_URL,
+})
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  })
+
+if (config.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,12 +2,15 @@ import { config } from 'dotenv'
 
 config()
 
+const NODE_ENV = process.env.NODE_ENV
+
 const PORT = process.env.PORT || 3001
 
 const DATABASE_URL = process.env.DATABASE_URL
 const DEV_DATABASE_URL = process.env.DEV_DATABASE_URL
 
 export default {
+  NODE_ENV,
   PORT,
   DATABASE_URL,
   DEV_DATABASE_URL,
