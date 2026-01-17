a4528babab307213f1df7ad3dde93fc5fc500f2e
sunny <goodluck14115@gmail.com>
2026-01-17 16:18:51 +0530

    Fix deploy error

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { ZodError } from 'zod'
-import { Prisma } from '../../app/generated/prisma/client'
+import { Prisma } from '../generated/prisma/client'
 import { HttpError } from '../utils/http-error'
 
 interface ErrorResponse {
